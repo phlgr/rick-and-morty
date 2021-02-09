29aeaedb3956c18ad5be114868e93c7188a25e3c
lmachens <leon@machens.koeln>
2021-02-09 12:36:46 +0100

    ✨ Add multiple cards story

--- src/components/card/card.stories.ts
+++ src/components/card/card.stories.ts
@@ -1,5 +1,6 @@
 import "./card.css";
 import { createCard } from "./card";
+import { createElement } from "../../utils/createElement";
 
 export default {
   title: "Components/Card",
@@ -23,3 +24,36 @@ export const Morty = () =>
     species: "Human",
     origin: { name: "Earth (C-137)" },
   });
+
+export const Multiple = () => {
+  const characters = [
+    {
+      imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      name: "Morty Smith",
+      status: "Dead",
+      species: "Human",
+      origin: { name: "Earth (C-137)" },
+    },
+    {
+      imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      name: "Rick Sanchenz",
+      status: "Alive",
+      species: "Human",
+      origin: { name: "Earth (C-137)" },
+    },
+    {
+      imgSrc: "https://rickandmortyapi.com/api/character/avatar/25.jpeg",
+      name: "Armothy",
+      status: "Dead",
+      species: "unknown",
+      origin: { name: "Post-Apocalyptic Earth" },
+    },
+  ];
+
+  const container = createElement("div", {
+    className: "container",
+    childs: characters.map((character) => createCard(character)),
+  });
+
+  return container;
+};
